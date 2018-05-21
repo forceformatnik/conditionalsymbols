519c094119a4739a0d79320a48e87c1fae137256
Alexandr Luhovtsov <aluho@tutanota.com>
2018-05-21 11:37:36 +0300

    first commit

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fl=new FolderBrowserDialog();
+            fl.ShowDialog();
+            string[] files = System.IO.Directory.GetFiles(fl.SelectedPath, "*.*", System.IO.SearchOption.AllDirectories);
+
+            foreach (var filename in files)
+            {
+                File.WriteAllText(filename, string.Format("{0}\n{1}", $"#if {textBox1.Text}", File.ReadAllText(filename)));
+            }
+
+            foreach (var filename in files)
+            {
+                File.AppendAllText(filename, string.Format("\n{0}", "#endif"));
+            }
+        }
+    }
+}
